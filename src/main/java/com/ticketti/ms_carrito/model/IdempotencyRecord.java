ffe9b493bf97d1eddeb58ab381dbf05409ea5126
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-26 17:10:04 -0400

    refactor(model): :recycle: Reorganizar importaciones en IdempotencyRecord

--- src/main/java/com/ticketti/ms_carrito/model/IdempotencyRecord.java
+++ src/main/java/com/ticketti/ms_carrito/model/IdempotencyRecord.java
@@ -1,11 +1,17 @@
 package com.ticketti.ms_carrito.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 /**
  * IdempotencyRecord - Entidad para garantizar idempotencia global. Almacena
