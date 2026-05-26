40539e62b38d8c1381927a9c18c0d4e9898dfbdd
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-26 17:11:07 -0400

    refactor(security): :recycle: Eliminar declaración de excepción en el método securityFilterChain

--- src/main/java/com/ticketti/ms_carrito/security/SecurityConfig.java
+++ src/main/java/com/ticketti/ms_carrito/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 	private final JwtAuthenticationFilter jwtAuthFilter;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 		http
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session
