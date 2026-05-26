36784df4590af773c4ca71c52cdb00f8b4de9682
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-26 17:08:43 -0400

    refactor(model): :recycle: Reorganizar importaciones en la clase Pedido

--- src/main/java/com/ticketti/ms_carrito/model/Pedido.java
+++ src/main/java/com/ticketti/ms_carrito/model/Pedido.java
@@ -1,14 +1,27 @@
 package com.ticketti.ms_carrito.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Pedido - Entidad principal del dominio (reemplaza `CarritoDeCompras`).
