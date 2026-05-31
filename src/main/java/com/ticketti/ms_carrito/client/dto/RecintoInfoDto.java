3f54fa586bd5f8a72005657558f8256e6f0fbe7a
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-31 15:49:01 -0400

    feat(dto): :sparkles: Agregar clase RecintoInfoDto para representar información de recintos

---
+++ src/main/java/com/ticketti/ms_carrito/client/dto/RecintoInfoDto.java
@@ -0,0 +1,11 @@
+package com.ticketti.ms_carrito.client.dto;
+
+import lombok.Data;
+
+@Data
+public class RecintoInfoDto {
+
+    private Integer id;
+    private String nombre;
+    private String ubicacion;
+}
