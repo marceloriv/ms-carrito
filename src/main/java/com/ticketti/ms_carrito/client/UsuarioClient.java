3108ffab5c83ef85a5ae39a487dbe8a5bb533293
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-31 15:45:40 -0400

    feat(client): :sparkles: Agregar cliente Feign para buscar información de usuarios

---
+++ src/main/java/com/ticketti/ms_carrito/client/UsuarioClient.java
@@ -0,0 +1,14 @@
+package com.ticketti.ms_carrito.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ticketti.ms_carrito.client.dto.UsuarioInfoDto;
+
+@FeignClient(name = "ms-usuarios", url = "${ms-usuarios.url:http://localhost:8082}")
+public interface UsuarioClient {
+
+    @GetMapping("/api/v1/usuarios/{usuarioId}")
+    UsuarioInfoDto buscarUsuario(@PathVariable Long usuarioId);
+}
