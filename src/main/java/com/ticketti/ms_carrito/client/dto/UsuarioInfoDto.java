3dcdf9027bfc7b07236c9f2a3405e5f565ad3286
marceloriv <ma.riveracontreras@duocuc.cl>
2026-05-31 15:49:39 -0400

    feat(dto): :sparkles: Agregar clase UsuarioInfoDto para representar información de usuarios

---
+++ src/main/java/com/ticketti/ms_carrito/client/dto/UsuarioInfoDto.java
@@ -0,0 +1,11 @@
+package com.ticketti.ms_carrito.client.dto;
+
+import lombok.Data;
+
+@Data
+public class UsuarioInfoDto {
+
+    private Long id;
+    private String nombre;
+    private String correo;
+}
