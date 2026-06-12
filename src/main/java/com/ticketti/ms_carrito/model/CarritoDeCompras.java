bdcdaae6702e96d1a311aaf561a676c45602f21d
marceloriv <ma.riveracontreras@duocuc.cl>
2026-06-12 19:40:05 -0400

    feat(carrito): :gift: hacer donación obligatoria del 10% y remover cargo por servicio

--- src/main/java/com/ticketti/ms_carrito/model/CarritoDeCompras.java
+++ src/main/java/com/ticketti/ms_carrito/model/CarritoDeCompras.java
@@ -98,7 +98,9 @@ public class CarritoDeCompras {
         this.subtotal = detalles.stream()
                 .map(DetalleCarrito::getSubtotal)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+        // La donación del 10% es siempre obligatoria
         this.montoDonacion = this.subtotal.multiply(new BigDecimal("0.10"));
+        
         this.total = this.subtotal.add(this.montoDonacion);
     }
 
